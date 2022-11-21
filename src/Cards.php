3555cc702389ea0353b52c49f0f80f9d6b74826c
Attia Ahmed <attia.a.ahmed@gmail.com>
2022-11-21 11:27:43 +0300

    fix

--- src/Cards.php
+++ src/Cards.php
@@ -8,7 +8,7 @@ class Cards extends Api
 	use \TapPayments\Requests\RetrieveForParent;
 	use \TapPayments\Requests\UpdateForParent;
 	use \TapPayments\Requests\AllForParent;
-    protected static $endpoint='/v2/cards';
+    protected static $endpoint='/v2/card';
     private static function classUrl()
     {
         return self::$endpoint;
