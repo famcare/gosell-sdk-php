0bf490daad4c682bdce671520edcc6180bf3f23e
Attia Ahmed <attia.a.ahmed@gmail.com>
2022-11-28 15:47:20 +0300

    refactor

--- src/Requests/Common.php
+++ src/Requests/Common.php
@@ -1,66 +1,49 @@
 <?php
 
 namespace TapPayments\Requests;
-use GuzzleHttp\Client as client;
-use GuzzleHttp\Psr7\Request;
-use GuzzleHttp\Psr7\Response;
+
+use Illuminate\Support\Facades\Http;
+
 /**
  * Trait for resources that need to make API requests.
  */
 trait Common
 {
 
-    public static function testingTrait(){
-        self::_validateParams('POST',[]);
+    public static function testingTrait()
+    {
+        self::_validateParams('POST', []);
     }
+
     protected static function _staticRequest($method, $url, $params, $options)
     {
-        try{
-            $headers = [
-            'Authorization' => 'Bearer '.static::getKey(),
-            'Content-Type'=>'application/json'
-            ];
-            
-            
-            if($params===null){
-                $request = new Request($method,  $url, $headers);
-            } else {
-                $body = json_encode($params);
-                $request = new Request($method,  $url, $headers, $body);
-            }
-            $guzzle = new client();
-            $response = $guzzle->send($request);
-            $jsonResponse = $response->getBody()->getContents();
-            
-            return json_decode($jsonResponse);
-        } catch (\GuzzleHttp\Exception\ClientException $e) {
-            $clientErrorResponse = $e->getResponse();
-            //echo (string)($response->getBody());exit;
-
-            return json_decode($clientErrorResponse->getBody());
-        } 
-    	
+        $body = $params === null ? null : json_encode($params);
+        return Http::withToken(static::getKey())
+            ->withBody($body, 'application/json')
+            ->send($method, $url)
+            ->throw()
+            ->object();
     }
 
-	/**
-     * @param array|null|mixed $params The list of parameters to validate
+    /**
+     * @param  array|null|mixed  $params  The list of parameters to validate
      *
      * @throws Exception if $params exists and is not an array
      */
-    protected static function _validateParams($method , $params = null)
+    protected static function _validateParams($method, $params = null)
     {
 
         if (empty($params) && !is_array($params)) {
-            $message = "You must pass an array as the ".($method=='POST')?'first':'second'." argument to GoSell API SDK"
-               . " create method calls.  (HINT: an example call to create a charge "
-               . "would be: \"GoSell\\Charges::create(['amount' => 100, "
-               . "'currency' => 'usd', 'source' => 'tok_1234'])\")";
+            $message = "You must pass an array as the ".($method == 'POST') ? 'first' : 'second'." argument to GoSell API SDK"
+                ." create method calls.  (HINT: an example call to create a charge "
+                ."would be: \"GoSell\\Charges::create(['amount' => 100, "
+                ."'currency' => 'usd', 'source' => 'tok_1234'])\")";
             throw new \Exception($message);
         }
     }
 
     /**
-     * @param array|null|mixed $params The list of parameters to validate
+     * @param  array|null|mixed  $params  The list of parameters to validate
      *
      * @throws Exception if $params exists and is not an array
      */
@@ -69,22 +52,22 @@ trait Common
 
         if (empty($params) && !is_array($params)) {
             $message = "You must pass first argument as string to GoSell API SDK"
-               . "to retrieve, update, delete method calls.  (HINT: an example call to create a charge "
-               . "would be: \"GoSell\\Charges::retieve('chg_Hw422020201340Jh9g0302671'])\")";
+                ."to retrieve, update, delete method calls.  (HINT: an example call to create a charge "
+                ."would be: \"GoSell\\Charges::retieve('chg_Hw422020201340Jh9g0302671'])\")";
             throw new \Exception($message);
         }
     }
 
-    
+
     protected static function _validateKey()
     {
-    	$key = static::getKey();
-    	//var_dump($key);exit;
-    	if(empty($key)){
-    		throw new \Exception("You must pass a private secret Key by calling setPrivateKey. Example: GoSell::setPrivateKey(\"sk_test_XKokBfNWv6FIYuTMg5sLPjhJ\")");
-    	}
-        if(strlen($key)!==32){
-        	throw new \Exception("Invalid private secret Key");
+        $key = static::getKey();
+        //var_dump($key);exit;
+        if (empty($key)) {
+            throw new \Exception("You must pass a private secret Key by calling setPrivateKey. Example: GoSell::setPrivateKey(\"sk_test_XKokBfNWv6FIYuTMg5sLPjhJ\")");
+        }
+        if (strlen($key) !== 32) {
+            throw new \Exception("Invalid private secret Key");
         }
     }
 }
